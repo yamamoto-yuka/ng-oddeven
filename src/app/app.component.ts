85f6923373c299146063b05862b5614c2a1e90f1
Yuka Yamamoto <yamamoto_yuka@hotmail.com>
2022-06-03 10:57:22 -0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'oddeven';
+  odd: any[] = [];
+  even: any[] = [];
+  x = 1;
+
+  ngOnInit(): void {
+    for (let i = 1; i <= 20; i++) {
+      if (i % 2 === 0) this.even.push(i);
+      else this.odd.push(i);
+    }
+
+    console.log(this.even);
+    console.log(this.odd);
+  }
 }
